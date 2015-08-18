6da629f0e8e27744b1dc9f2b3e41018e7180e865
ovmakarova <ss2015@ws-2k-110-n10.ccam.cmc.unn.net>
2015-08-18 14:40:10 +0300

    change place

---
+++ sample_Makarova/processing.cpp
@@ -0,0 +1,29 @@
+#include "processing.hpp"
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <time.h>
+#include <math.h>
+
+using namespace cv;
+
+void Processing::processFrame(const cv::Mat& src, cv::Mat& dst)
+{
+    src.copyTo(dst);
+
+    srand(time(NULL));
+    int x0 = rand()%src.rows-1;
+    int y0 = rand()%src.cols-1;
+
+    while((x0+x0+100 > 500) || (y0+y0+100> 500)){
+        x0 = rand()%src.rows-1;
+        y0 = rand()%src.cols-1;
+    }
+
+    cv::Rect region(x0, y0, x0+100, y0+100);
+    Mat roi = dst(region);
+
+    const int kSize = 11;
+    medianBlur(roi, roi, kSize);
+
+    rectangle(dst, region, Scalar(255, 0, 0));
+}
