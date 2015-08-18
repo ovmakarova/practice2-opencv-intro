dabf25292ee1a49a680dcebd550648bc58abbf4a
ovmakarova <ss2015@ws-2k-110-n10.ccam.cmc.unn.net>
2015-08-18 15:33:54 +0300

    change place without mistakes

--- sample_Makarova/processing.cpp
+++ sample_Makarova/processing.cpp
@@ -15,8 +15,8 @@ void Processing::processFrame(const cv::Mat& src, cv::Mat& dst)
     int y0 = rand()%src.cols;
 
     while((x0+x0+100 > 500) || (y0+y0+100> 500)){
-        x0 = rand()%src.rows-1;
-        y0 = rand()%src.cols-1;
+        x0 = rand()%src.rows;
+        y0 = rand()%src.cols;
     }
 
     cv::Rect region(x0, y0, x0+100, y0+100);
