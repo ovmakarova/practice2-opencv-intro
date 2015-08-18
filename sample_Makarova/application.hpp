faf18031c81d6bf5c64ef5636120309916f1d79a
ovmakarova <ss2015@ws-2k-110-n10.ccam.cmc.unn.net>
2015-08-18 15:36:40 +0300

    add butten

---
+++ sample_Makarova/application.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <opencv2/core/core.hpp>
+
+#include "processing.hpp"
+
+bool onButtonClicked(cv::Rect buttonPlace, int x, int y);
+void onButtonsOnOffClick(int eventId, int x, int y, int flags, void *userData);
+
+class Application
+{
+ public:
+    enum WindowState
+    {
+        OnFilter,
+        OffFilter,
+        OnSave
+    };
+    struct Parameters
+    {
+        std::string imgFileName;
+    };
+    struct GUIElementsState
+    {
+        WindowState state;
+        cv::Rect onButtonPlace;
+        cv::Rect offButtonPlace;
+        cv::Rect onSavePlace;
+    };
+    int parseArguments(int argc, const char **argv, Parameters &params);
+    int getFrame(const std::string &fileName, cv::Mat& src);
+    int processFrame(const cv::Mat& src, cv::Mat& dst);
+    int showFrame(const std::string &caption, 
+                  const cv::Mat& src, cv::Mat& dst);
+    friend void onButtonsOnOffClick(int eventId, int x, int y, 
+                                    int flags, void *userData);
+    Application() 
+    { 
+        guiState.state = OnFilter;
+        isOnSave = false;
+    };
+
+ private:
+    Processing processor;
+    GUIElementsState guiState;
+
+    bool isOnSave;
+    int drawButtons(cv::Mat &display);
+    
+    friend bool onButtonClicked(cv::Rect buttonPlace, int x, int y);
+};
